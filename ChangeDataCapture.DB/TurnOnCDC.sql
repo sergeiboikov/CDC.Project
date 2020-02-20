844c4f2f7c5e07440aa37b1abc92587faed8f191
Sergei Boikov <Sergei_boikov@epam.com>
2020-02-20 10:27:04 +0400

    Created DB for CDC example

---
+++ ChangeDataCapture.DB/TurnOnCDC.sql
@@ -0,0 +1,27 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+:setvar DBName "ChangeDataCapture"
+
+IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE [name] =  N'$(DBName)' AND [is_cdc_enabled] = 1)
+BEGIN
+	BEGIN TRAN
+		EXEC sys.sp_cdc_enable_db;
+
+		EXEC sys.sp_cdc_enable_table
+			@source_schema = 'dbo', --Schema of Source Table
+			@source_name = 'cdc_states', --Schema of Source Table
+			@role_name = NULL, --Controls Access to Change Data
+			@supports_net_changes = 0 --Supports Quering for net changes
+	COMMIT TRAN
+END
+
+
---
+++ ChangeDataCapture.DB/cdc_states.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[cdc_states]
+(
+	[name]  [nvarchar](256) NOT NULL,
+	[state]	[nvarchar](256)	NOT NULL) ON [PRIMARY];
+
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [cdc_states_name] ON
+[dbo].[cdc_states]
+([name] ASC)
+WITH (PAD_INDEX = OFF) ON [PRIMARY]
